61d1a51b1a85100a116efdcf4631b8067566f6f1
Mahmoud Fathi <xenammm80@gmail.com>
2023-07-17 04:49:33 +0200

    basic structure for job app and register it in admin panel

--- project/job/models.py
+++ project/job/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Job(models.Model):
+    job_type_choices = (('Full Time', 'Full Time'),
+                        ('Part Time', 'Part Time'))
+    title = models.CharField(max_length=150)
+    # location
+    job_type = models.CharField(max_length=30, choices=job_type_choices)
+    description = models.TextField(max_length=500)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.DecimalField(default=0.00, decimal_places=2, max_digits=8)
+    experience = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
