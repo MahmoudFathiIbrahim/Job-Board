a0062b21408c3ae6c1853c123d40283e492c795a
Mahmoud Fathi <xenammm80@gmail.com>
2023-07-17 05:11:20 +0200

    create views and urls for job app

--- project/job/models.py
+++ project/job/models.py
@@ -2,6 +2,8 @@ from django.db import models
 # Create your models here.
 
 
+
+
 class Job(models.Model):
     job_type_choices = (('Full Time', 'Full Time'),
                         ('Part Time', 'Part Time'))
@@ -13,6 +15,15 @@ class Job(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.DecimalField(default=0.00, decimal_places=2, max_digits=8)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
--- project/job/views.py
+++ project/job/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def job_list(request):
+    return HttpResponse('welcome job board')
+
+
+def job_detail(request, job_id):
+    pass
